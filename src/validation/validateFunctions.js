d2834d4ea6680b9aaef7ff4fd04630988fb2f591
dlsxjzld <sinjuk1@naver.com>
2024-12-11 14:29:38 +0900

    feat: 월과 요일 입력 검증 추가

--- src/validation/validateFunctions.js
+++ src/validation/validateFunctions.js
@@ -1,4 +1,5 @@
 import { CONSTANT } from '../constants/constant.js';
+import { ERROR_MESSAGE } from '../constants/message.js';
 
 export const toThrowNewError = (condition, errorMessage) => {
   if (condition) {
@@ -9,18 +10,46 @@ export const toThrowNewError = (condition, errorMessage) => {
 const hasEmptySpace = (input) => [
   toThrowNewError(
     input.includes(' '),
-    '유효하지 않은 날짜입니다. 다시 입력해 주세요. 공백은 안됩니다.',
+    `${ERROR_MESSAGE.MONTH_DAY} 공백은 안됩니다.`,
   ),
 ];
 
 const isEmptyString = (input) => {
   toThrowNewError(
     input === '',
-    '유효하지 않은 날짜입니다. 다시 입력해 주세요. 빈 문자열은 안됩니다.',
+    `${ERROR_MESSAGE.MONTH_DAY} 빈 문자열은 안됩니다.`,
   );
 };
 
-export const validateSomething = (input) => {
+export const checkMonth = (input) => {
+  const [month, day] = input.split(',');
+  toThrowNewError(
+    CONSTANT.MONTH.includes(month) === false,
+    `${ERROR_MESSAGE.MONTH_DAY} 1~12월만 입력해주세요.`,
+  );
+};
+export const checkDay = (input) => {
+  const [month, day] = input.split(',');
+  toThrowNewError(
+    CONSTANT.DAY.includes(day) === false,
+    `${ERROR_MESSAGE.MONTH_DAY} (일, 월, 화, 수, 목, 금, 토)중에 하나만 입력해주세요.`,
+  );
+};
+export const checkCount = (input) => {
+  const charCount = input.split(',').filter(Boolean).length;
+  const delimiterCount = input
+    .split('')
+    .filter((value) => value === ',').length;
+  toThrowNewError(
+    charCount !== 2 || delimiterCount !== 1,
+    `${ERROR_MESSAGE.MONTH_DAY}`,
+  );
+};
+
+export const validateMonthAndDay = (input) => {
   hasEmptySpace(input);
   isEmptyString(input);
+  checkCount(input);
+  checkMonth(input);
+  checkDay(input);
 };
