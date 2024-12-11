31b7326aa82891f37d23c0d4c22553987cdce99b
dlsxjzld <sinjuk1@naver.com>
2024-12-11 14:29:56 +0900

    feat: 월과 요일 입력 연결

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import { InputView } from './view/InputView.js';
 import { OutputView } from './view/OutputView.js';
 import { MESSAGE } from './constants/message.js';
-import {} from './validation/validateFunctions.js';
+import { validateMonthAndDay } from './validation/validateFunctions.js';
 
 class App {
-  async run() {}
+  async run() {
+    const monthAndDay = await this.getMonthAndDay();
+    console.log(monthAndDay);
+  }
+
+  async getMonthAndDay() {
+    try {
+      const input = await InputView.readUserInput(MESSAGE.ASK_MONTH_DAY);
+      validateMonthAndDay(input);
+      return input;
+    } catch (error) {
+      OutputView.printResult(error.message);
+      return this.getMonthAndDay();
+    }
+  }
 }
 
 export default App;
