101e5fda599bb37d3807f0486c634ef063064ec2
dlsxjzld <sinjuk1@naver.com>
2024-12-11 14:05:39 +0900

    feat: 입력 및 에러 메시지 상수 추가

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,17 +1,9 @@
 export const MESSAGE = {
-  // EVENT_PLANNER_INSTRUCTION:
-  //   '안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n',
-  // ASK_VISIT_DATE:
-  //   '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n',
-  // ASK_MENU:
-  //   '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n',
-  // EVENT_PLANNER: (month, day) =>
-  //   `${month}월 ${day}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n`,
-  // ORDER_MENU: '<주문 메뉴>',
-  // BEFORE_DISCOUNT: '<할인 전 총주문 금액>',
-  // FREE_GIFT: '<증정 메뉴>',
-  // BENEFIT_DETAILS: '<혜택 내역>',
-  // TOTAL_DISCOUNT: '<총혜택 금액>',
-  // AFTER_DISCOUNT: '<할인 후 예상 결제 금액>',
-  // DECEMBER_BADGE: '<12월 이벤트 배지>',
+  ASK_MONTH_DAY: '비상 근무를 배정할 월과 시작 요일을 입력하세요> ',
+  ASK_WEEKDAY_MAN: '평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ',
+  ASK_HOLIDAY_MAN: '휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ',
+};
+
+export const ERROR_MESSAGE = {
+  MONTH_DAY: '유효하지 않은 입력 값입니다. 다시 입력해 주세요.',
 };
