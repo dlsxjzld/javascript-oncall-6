dd3da55f0432c326b6e7e675fcafc7b0306045c1
dlsxjzld <sinjuk1@naver.com>
2024-12-11 14:05:21 +0900

    feat: 월별 일수, 요일, 월 상수 추가

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,6 +1,18 @@
 export const CONSTANT = {
-  // MIN_VISIT_RANGE: 1,
-  // MAX_VISIT_RANGE: 31,
-  // MIN_QUANTITY_RANGE: 1,
-  // MAX_QUANTITY_RANGE: 20,
+  MONTH: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+  DAY: ['일', '월', '화', '수', '목', '금', '토'],
+  DATE: {
+    1: 31,
+    2: 28,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31,
+  },
 };
